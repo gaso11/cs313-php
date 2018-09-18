1df9cddd8ad6ea6bf3a29354e3f5169c57a26337
gaso11 <lew14017@byui.edu>
2018-09-18 11:36:53 -0600

    Completed Homepage, extra things: List buttons that work on hover, instead of just the text.

--- web/home.php
+++ web/home.php
@@ -1,34 +1,45 @@
 <html>
 <head>
-	<link rel="stylesheet" href="homeStyle.css">
-	<title>
-		Evan's Homepage
-	</title>
-</head>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" type="text/css" href="homeStyle.css">
+  <title>Evan's Homepage</title>
 <body>
-	<h1>Evan's Homepage</h1>
+  <div class="header">
+    <h1>Evan's Homepage</h1>
+  </div>
 
-	<ul>
-  		<li><a class="active" href="home.php">Home</a></li>
-  		<li><a href="assignments.html">Assignments</a></li>
-	</ul>
-	<br>
+  <!--Menu-->
+  <div class="row">
+    <div class="col-3 menu">
+      <ul>
+      	<li onclick="location.href = 'home.php';">Home</li>
+        <li onclick="location.href = 'assignments.html';">Assignments</li>
+      </ul>
+    </div>
 
-	<div class="centered">
-		<button class="button shadows centered"><a href="assignments.html">Assignments</a></button>
-	</div>
+    <!-- Body -->
+    <div class="col-9">
+    	<img src="stc.jpg" alt="Image of the STC building">
+      <h1>Interests</h1>
+        <p>
+          I don't really have interests that trump something else. I enjoy playing video games<br>
+          and watching movies with my wife. I picked the STC as my image because it's my favorite<br>
+          building on campus and I have a lot of good memories from working on projects with my friends.
+        </p>
+    </div>
+  </div>
+  <div class="footer">
+    <footer>
+      <!---PHP Time -->
+      <?php
+      	// Stackoverflow: How to get date and time from server
+      	date_default_timezone_set('America/Denver');
+      	
+      	$currentDate = date('m/d/Y - H:i:s');
 
-
-	<!--Push to bottom of page-->
-	<br>
-	<br>
-	<br>
-	<br>
-	<!-- PHP doing "something" -->
-	<?php
-
-  		echo "Server time: " . date("D M d, Y G:i a");
-
-	?>
+      	echo "Current time on load: " . $currentDate;
+      ?>
+    </footer>
+  </div>
 </body>
 </html>
