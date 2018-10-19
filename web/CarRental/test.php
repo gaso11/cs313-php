a992de363b5a762c47462c6000e5e266fffb4433
gaso11 <lew14017@byui.edu>
2018-10-19 17:05:23 -0600

    Added test page so I can ruin that page

---
+++ web/CarRental/test.php
@@ -0,0 +1,90 @@
+<!-- THIS WILL ONLY WORK WHEN PUSHED TO HEROKU -->
+<?php
+
+function dbConnect(){
+    try {
+      $url = getenv('DATABASE_URL');
+      $opts = parse_url($url);
+      $server = $opts["host"];
+      $database = ltrim($opts["path"],'/');
+      $user = $opts["user"];
+      $password = $opts["pass"];
+      $port = $opts["port"];
+      $dsn = "pgsql:host=$server; port=$port;dbname=$database";
+      $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+      $link = new PDO($dsn, $user, $password, $options);
+      return $link;
+    }
+    catch (PDOException $ex) {
+      echo $ex;
+      exit;
+    }
+  }
+
+function getCarsDB() {
+    $db = dbConnect();
+    $sql = "SELECT * FROM Cars";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_NAMED);
+    $stmt->closeCursor();
+    return $data;
+  }
+  
+$cars = getCarsDB();
+
+?>
+
+<html>
+<head>
+    <title>Car Rental Service</title>
+    <link rel="stylesheet" href="carRentalBrowse.css" type="text/css">
+</head>
+<!--Header from https://codepen.io/linux/pen/aEQKWP -->
+<header>
+    <div class="header">
+        <h1>*TEST PAGE*</h1><br>
+        <h3>At new all low prices!</h3>
+        <br><br>
+        <button><a href="empcar.php">Login</a></button>
+    </div>
+</header> 
+<body>
+    
+    <!-- Searching -->
+    <form action="carRentalBrowse.php" method="post">
+        <input type="text" name="search" placeholder="Search for cars..."/>
+        <input type="submit" value=">>"/>
+    </form>
+    
+    <div class="carTable">
+        <h2>Cars Avaliable for Rent</h2>
+        <ul class="table">
+            <li class="table-header">
+                <div class="col col-1">Make</div>
+                <div class="col col-2">Model</div>
+                <div class="col col-3">Cost per day</div>
+            </li>
+            <?php
+    
+            foreach($cars as $car) 
+            {
+                if ($car['repairstatus'] == "Okay" && $car['rentalstatus'] == "Open")
+                {
+                    
+                echo "<li class=\"table-row\">";
+                echo "<div class=\"col col-1\" data-label=\"Make\">" . 
+                    $car['make'] . "</div>";
+                echo "<div class=\"col col-2\" data-label=\"Model\">" . 
+                    $car['model'] . "</div>";
+                echo "<div class=\"col col-3\" data-label=\"Cost\">" . 
+                    "$" . $car['cost'] . "</div>";
+                echo "</li>";
+                }
+            }
+            
+            ?>
+        </ul>
+    </div>
+    <br>
+</body>
