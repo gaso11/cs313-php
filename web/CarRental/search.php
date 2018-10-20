a969e7110ed7522d67c8b7f5184d64126ed19985
gaso11 <lew14017@byui.edu>
2018-10-19 19:30:02 -0600

    Gonna try ajax 2

--- web/CarRental/search.php
+++ web/CarRental/search.php
@@ -1,3 +1,19 @@
 <?php
-    echo "called";
+    if (empty($_POST['search']))
+    {
+        echo "Nothing to search";
+        return;
+    }
+    else
+    {
+        $search = $_POST['search']
+        $db = dbConnect();
+        $sql = "SELECT * FROM Cars WHERE 
+                make LIKE '%$search%' OR model LIKE '%$search%'";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $data = $stmt->fetchAll(PDO::FETCH_NAMED);
+        $stmt->closeCursor();
+        echo json_encode($data);
+    }
 ?>
