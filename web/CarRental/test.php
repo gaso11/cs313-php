ac23543912aa4196178de53dba2953e23205eebf
gaso11 <lew14017@byui.edu>
2018-10-19 19:00:37 -0600

    Trying a search again, probably won't work

--- web/CarRental/test.php
+++ web/CarRental/test.php
@@ -23,12 +23,34 @@ function dbConnect(){
 
 function getCarsDB() {
     $db = dbConnect();
-    $sql = "SELECT * FROM Cars";
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    $data = $stmt->fetchAll(PDO::FETCH_NAMED);
-    $stmt->closeCursor();
-    return $data;
+    $output = '';
+    
+    if (isset($_POST['search']))
+    {
+        $searchq = $_POST['search'];
+        $searchq = preg_replace("#[^0-9a-z]#i", "", $searchq);
+        
+        $stmt = $db->prepare("SELECT * FROM Cars WHERE 
+                            make LIKE '%$searchq%' OR model LIKE '%$searchq%'");
+        $stmt->execute();
+        $count = pg_num_rows($stmt);
+        if ($count == 0)
+        {
+            $output = 'No Results';
+        } 
+        else 
+        {
+            while($rows = $stmt->fetchALL(PDO::FETCH_ASSOC))
+            {
+                $make = $rows['make'];
+                $model = $rows['model'];
+                
+                $output .= '<div> ' .$make. ' ' .$model.'</div>';
+            }
+        }
+        
+    }
+    
   }
   
 $cars = getCarsDB();
@@ -52,39 +74,12 @@ $cars = getCarsDB();
 <body>
     
     <!-- Searching -->
-    <form action="carRentalBrowse.php" method="post">
+    <form action="carRentalBrowse.php" method="POST">
         <input type="text" name="search" placeholder="Search for cars..."/>
         <input type="submit" value=">>"/>
     </form>
     
-    <div class="carTable">
-        <h2>Cars Avaliable for Rent</h2>
-        <ul class="table">
-            <li class="table-header">
-                <div class="col col-1">Make</div>
-                <div class="col col-2">Model</div>
-                <div class="col col-3">Cost per day</div>
-            </li>
-            <?php
+    <?php print("$output"); ?>
     
-            foreach($cars as $car) 
-            {
-                if ($car['repairstatus'] == "Okay" && $car['rentalstatus'] == "Open")
-                {
-                    
-                echo "<li class=\"table-row\">";
-                echo "<div class=\"col col-1\" data-label=\"Make\">" . 
-                    $car['make'] . "</div>";
-                echo "<div class=\"col col-2\" data-label=\"Model\">" . 
-                    $car['model'] . "</div>";
-                echo "<div class=\"col col-3\" data-label=\"Cost\">" . 
-                    "$" . $car['cost'] . "</div>";
-                echo "</li>";
-                }
-            }
-            
-            ?>
-        </ul>
-    </div>
     <br>
 </body>
