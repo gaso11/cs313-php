32f514c971095bd8c69aaa02ab7726bca7cd7e12
gaso11 <lew14017@byui.edu>
2018-10-19 21:00:45 -0600

    Trying PHP again

--- web/CarRental/search.php
+++ web/CarRental/search.php
@@ -27,12 +27,13 @@
     {
         $search = $_POST["search"]
         $db = dbConnect();
+        /*
         $sql = "SELECT * FROM Cars WHERE 
                 make LIKE '%$search%' OR model LIKE '%$search%'";
         $stmt = $db->prepare($sql);
         $stmt->execute();
         $data = $stmt->fetchAll(PDO::FETCH_NAMED);
         $stmt->closeCursor();
-        echo json_encode($data);
+        echo json_encode($data);*/
     }
 ?>
