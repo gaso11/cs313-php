c9f72a58e93849a18a8a8928f0a12a0276b9d9fa
gaso11 <lew14017@byui.edu>
2018-10-24 14:38:52 -0600

    Fixed text

--- web/ta07/login.php
+++ web/ta07/login.php
@@ -18,14 +18,14 @@
       <div class="loginMsg">
         <div class="textcontent">
           <p class="title">Don't have an account?</p>
-          <p>Sign up to save all your graph.</p>
+          <p>Sign up to save yourself.</p>
           <button id="switch1">Sign Up</button>
         </div>
       </div>
       <div class="signupMsg visibility">
         <div class="textcontent">
           <p class="title">Have an account?</p>
-          <p>Log in to see all your collection.</p>
+          <p>Log in to see a boring screen.</p>
           <button id="switch2">LOG IN</button>
         </div>
       </div>
