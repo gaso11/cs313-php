83e5581e886d639cb9c8b7d6b4916748e0d339b9
gaso11 <lew14017@byui.edu>
2018-10-25 14:24:29 -0600

    Starting work on update

--- web/CarRental/update.php
+++ web/CarRental/update.php
@@ -60,7 +60,7 @@ function getCarsDB() {
     
 /* Make list of cars to remove */
 echo "<form class=\"addform\" action=\"updateq.php\" method=\"POST\">";
-echo "<div>"
+echo "<div>";
 echo "<select name=\"carList\" id=\"carList\">";
 foreach($cars as $car) 
 {
