919f1dfb06629617a3e56acf800326e5e9ce15b8
gaso11 <lew14017@byui.edu>
2018-10-25 17:23:31 -0600

    Making sure variables work

--- web/CarRental/updateq.php
+++ web/CarRental/updateq.php
@@ -1,6 +1,51 @@
 <?php
-  
+
+if($_SESSION['verified'])
+{
+    $cars = getCarsDB();
+    getVars();
+}
+else
+{
+    header("Location: carRentalBrowse.php");
+}
+
+function dbConnect(){
+    try {
+      $url = getenv('DATABASE_URL');
+      $opts = parse_url($url);
+      $server = $opts["host"];
+      $database = ltrim($opts["path"],'/');
+      $user = $opts["user"];
+      $password = $opts["pass"];
+      $port = $opts["port"];
+      $dsn = "pgsql:host=$server; port=$port;dbname=$database";
+      $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+      $link = new PDO($dsn, $user, $password, $options);
+      return $link;
+    }
+    catch (PDOException $ex) {
+      echo $ex;
+      exit;
+    }
+  }
+
+function getCarsDB() {
+    $db = dbConnect();
+    $sql = "SELECT * FROM Cars";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $data = $stmt->fetchAll(PDO::FETCH_NAMED);
+    $stmt->closeCursor();
+    return $data;
+  }
+
+function getVars()
+{
     $carID = $_POST['carList'];
     echo "CarID: $carID";
+    $make = $cars[$carID];
+    echo "Make: $make";
+}
 
 ?>
