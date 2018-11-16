a268f7515212b72a76ab02450c0dda386386eb91
gaso11 <lew14017@byui.edu>
2018-11-15 21:36:01 -0700

    Tweaked server.js to create a path?

--- web/postal/server.js
+++ web/postal/server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("public"));
-app.set("views", "views");
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
     console.log("Recieved request for root");
